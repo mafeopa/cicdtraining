28ee3d0a402f64965839b97449bd0f33e52cfeff
mafeopa <mfochoa@unibopyaca.edu.co>
2026-06-04 18:06:59 -0600

    feature: change message app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('Hola desde Cloud Run 🚀')
+  res.send('Hola desde Cloud Run 040620261908🚀')
 })
 
 app.listen(PORT, '0.0.0.0', () => {
